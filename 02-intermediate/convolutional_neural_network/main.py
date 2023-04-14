c7e1abd8a612cbda47bb09ae6555cd1b1e0b549d
ZHymLumine <anka753997@gmail.com>
2023-04-14 23:44:49 +0900

    02

--- 02-intermediate/convolutional_neural_network/main.py
+++ 02-intermediate/convolutional_neural_network/main.py
@@ -5,27 +5,25 @@ import torchvision.transforms as transforms
 
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# Hyper-parameters 
-input_size = 784
-hidden_size = 500
-num_classes = 10
+# Hyper parameters
 num_epochs = 5
+num_classes = 10
 batch_size = 100
 learning_rate = 0.001
 
-# MNIST dataset (images and labels)
-train_dataset = torchvision.datasets.MNIST(root='../../data', 
-                                           train=True, 
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data',
+                                           train=True,
                                            transform=transforms.ToTensor(),
                                            download=True)
 
 test_dataset = torchvision.datasets.MNIST(root='../../data',
-                                          train=False,
+                                          train=True,
                                           transform=transforms.ToTensor())
 
-# Data loader (input pipeline)
+# Dataloader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
@@ -34,31 +32,40 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
-# Fully connected neural network with one hidden layer
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet , self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7 * 7 * 32, num_classes)
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
         return out
     
-model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+model = ConvNet(num_classes).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        images = images.reshape(-1, input_size).to(device)
+        images = images.to(device)
         labels = labels.to(device)
 
         # Forward pass
@@ -73,14 +80,14 @@ for epoch in range(num_epochs):
         if (i + 1) % 100 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-
-# Test the model
-# In test phase, we don't need to compute gradients (for memory efficiency)
+            
+# Test the model In test phase, we don't need to compute gradients (for memory efficiency)
+model.eval() # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
 with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
-        images = images.reshape(-1, input_size).to(device)
+        images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
